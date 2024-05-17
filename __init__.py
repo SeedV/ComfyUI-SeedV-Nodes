fcfff39684b87a088379b47e53bce92caeb0d511
RalphHan <hanhongwei_hf@qq.com>
2024-05-17 15:40:12 +0800

    add LoraLoader //SeedV

--- __init__.py
+++ __init__.py
@@ -1,14 +1,16 @@
 from .script import Script
-from .shared_ckpt import CheckpointLoaderSimpleShared
+from .shared_ckpt import CheckpointLoaderSimpleShared, LoraLoader
 
 NODE_CLASS_MAPPINGS = {
     "Script": Script,
     "CheckpointLoaderSimpleShared //SeedV": CheckpointLoaderSimpleShared,
+    "LoraLoader //SeedV": LoraLoader,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Script": "Script",
     "CheckpointLoaderSimpleShared //SeedV": "Shared Checkpoint Loader (SeedV)",
+    "LoraLoader //SeedV": "Load LoRA (SeedV)",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
