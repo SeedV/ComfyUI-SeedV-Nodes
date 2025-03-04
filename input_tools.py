179047ed360e790adceba6419137c10ab7136880
AQFU <281117192@qq.com>
2025-03-04 14:48:25 +0800

    change to LoadAudioOrBase64

--- input_tools.py
+++ input_tools.py
@@ -2,6 +2,8 @@ import base64
 import io
 import torchaudio
 import hashlib
+import folder_paths
+import os
 
 def process_audio_base64(audio_base64):
     audio_bytes = base64.b64decode(audio_base64)
@@ -9,31 +11,46 @@ def process_audio_base64(audio_base64):
     audio_bytes, sample_rate = torchaudio.load(audio_io)
     return audio_bytes, sample_rate
 
-class LoadAudioBase64:
+class LoadAudioOrBase64:
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"audio": ("STRING", {"multiline": False})}}
+        input_dir = folder_paths.get_input_directory()
+        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        return {
+                "required": 
+                    {
+                        "audio": (sorted(files), {"audio_upload": True}),
+                        "audio_base64": ("STRING", {"multiline": False})
+                    }
+                }
 
     RETURN_TYPES = ("AUDIO",)
     CATEGORY = "_external_tooling"
     FUNCTION = "load_audio"
 
-    def load_audio(self, audio_base64_str):
-        audio_bytes, sample_rate = process_audio_base64(audio_base64_str)
+    def load_audio(self, audio, audio_base64):
+        if audio_base64:
+            audio_bytes, sample_rate = process_audio_base64(audio_base64)
+        else:
+            audio_path = folder_paths.get_annotated_filepath(audio)
+            audio_bytes, sample_rate = torchaudio.load(audio_path)
         audio = {"waveform": audio_bytes.unsqueeze(0), "sample_rate": sample_rate}
-        return audio
+        return (audio,)
     
     @classmethod
-    def IS_CHANGED(s, audio_base64_str):
+    def IS_CHANGED(s, audio, audio_base64):
+        if not audio_base64:
+            audio_path = folder_paths.get_annotated_filepath(audio)
+            with open(audio_path, "rb") as audio_file:
+                audio_bytes = audio_file.read()
+        else:
+            audio_bytes = base64.b64decode(audio_base64)
         m = hashlib.sha256()
-        m.update(audio_base64_str)
+        m.update(audio_bytes)
         return m.digest().hex()
 
     @classmethod
-    def VALIDATE_INPUTS(s, audio):
-        try:
-            process_audio_base64(audio)
-            return True
-        except Exception as e:
-            return False
-
+    def VALIDATE_INPUTS(s, audio, audio_base64):
+        if not audio_base64 and not folder_paths.exists_annotated_file(audio):
+            return "Invalid audio input"
+        return True
