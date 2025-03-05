0cefb30e70647a841f3d5ff1d91675c17bcb345a
AQFU <281117192@qq.com>
2025-03-05 14:13:15 +0800

    fix INPUT_TYPES

--- __init__.py
+++ __init__.py
@@ -15,7 +15,8 @@ NODE_CLASS_MAPPINGS = {
     "ALL_Model_UnLoader(SEEDV)": ModelUnloader,
     "Switch_Any(SEEDV)": Switch_Any,
     "TCD_Sampler(SEEDV)": TCDModelSamplingDiscrete,
-    "LoadAudioOrBase64": LoadAudioOrBase64
+    "LoadAudioOrBase64": LoadAudioOrBase64，
+    “LoadAudioBase64”: LoadAudioBase64
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -27,7 +28,8 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "ALL_Model_UnLoader(SEEDV)": "ALL_Model_UnLoader(SEEDV)",
     "Switch_Any(SEEDV)": "Switch_Any(SEEDV UnSafe)",
     "TCD_Sampler(SEEDV)": "TCD_Sampler(SEEDV)",
-    "LoadAudioOrBase64": "Load Audio or Base64"
+    "LoadAudioOrBase64": "Load Audio or Base64"，
+    "LoadAudioBase64": "Load Audio Base64"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
--- input_tools.py
+++ input_tools.py
@@ -15,17 +15,17 @@ class LoadAudioOrBase64:
     @classmethod
     def INPUT_TYPES(s):
         input_dir = folder_paths.get_input_directory()
-        files = [f for f in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, f))]
+        files = folder_paths.filter_files_content_types(os.listdir(input_dir), ["audio", "video"])
         return {
                 "required": 
                     {
-                        "audio": (sorted(files), {"audio_upload": True}),
+                        "audio": (sorted(files), ),
                         "audio_base64": ("STRING", {"multiline": False})
-                    }
+                    },
                 }
 
     RETURN_TYPES = ("AUDIO",)
-    CATEGORY = "_external_tooling"
+    CATEGORY = "SeedV"
     FUNCTION = "load_audio"
 
     def load_audio(self, audio, audio_base64):
@@ -53,4 +53,34 @@ class LoadAudioOrBase64:
     def VALIDATE_INPUTS(s, audio, audio_base64):
         if not audio_base64 and not folder_paths.exists_annotated_file(audio):
             return "Invalid audio input"
+        return True
+    
+class LoadAudioBase64:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+                "required": 
+                    {
+                        "audio_base64": ("STRING", {"multiline": False})
+                    },
+                }
+
+    RETURN_TYPES = ("AUDIO",)
+    CATEGORY = "SeedV"
+    FUNCTION = "load_audio"
+
+    def load_audio(self, audio_base64):
+        audio_bytes, sample_rate = process_audio_base64(audio_base64)
+        audio = {"waveform": audio_bytes.unsqueeze(0), "sample_rate": sample_rate}
+        return (audio,)
+    
+    @classmethod
+    def IS_CHANGED(s, audio_base64):
+        audio_bytes = base64.b64decode(audio_base64)
+        m = hashlib.sha256()
+        m.update(audio_bytes)
+        return m.digest().hex()
+
+    @classmethod
+    def VALIDATE_INPUTS(s, audio_base64):
         return True
